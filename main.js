e34737163b660800bd893094fd8aeaafa11ee3e0
havertzz04 <havertzz04@gmail.com>
2023-07-04 08:38:57 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+let minutesTimer = document.querySelector("#minutes");
+let secondsTimer = document.querySelector("#seconds");
+
+minutes = 0;
+seconds = 0;
+
+function pomodoroTimer(){
+    seconds++;
+    if(seconds === 60){
+        seconds = 0;
+        minutes++;
+    }
+
+    const setMinutes = String(minutes).padStart(2, "0");
+    const setSeconds = String(seconds).padStart(2, "0");
+
+    minutesTimer.textContent = setMinutes;
+    secondsTimer.textContent = setSeconds;
+}
+
+setInterval(pomodoroTimer, 1000)
